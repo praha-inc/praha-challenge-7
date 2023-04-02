f31f1f6e306ac4284f1507be7da1d7087a1e6d5f
YamazakiYusuke <yusuke38098@gmail.com>
2023-04-02 12:19:30 +0900

    サンプルSQLをそのまま実行して値が取得できるように修正

--- 95IG7y3HHX7yl4/01_データベース(設計と基礎)/02_DBモデリング2/DML_use_case.sql
+++ 95IG7y3HHX7yl4/01_データベース(設計と基礎)/02_DBモデリング2/DML_use_case.sql
@@ -1,76 +1,76 @@
 --- CREATE, DELETE, UPDATE
 -- Userの作成
-INSERT INTO users (username, email, password, display_name, profile_image_url) VALUES
-('Yamazaki Yusuke', 'yusuke@example.com', 'password_yusuke', 'ゆうすけ', 'https://picsum.photos/200');
+INSERT INTO users (id, username, email, password, display_name, profile_image_url) VALUES
+(100, 'Yamazaki Yusuke', 'yusuke@example.com', 'password_yusuke', 'ゆうすけ', 'https://picsum.photos/200');
 -- Userの編集
 UPDATE users
 SET email = 'yusuke@gmail.com'
-WHERE id = :user_id;
+WHERE id = 100;
 -- Userの削除
 DELETE FROM users
-WHERE id = :user_id;
+WHERE id = 100;
 
 -- ワークスペースの作成
-INSERT INTO workspaces (name) VALUES
-('プラハチャレンジ');
+INSERT INTO workspaces (id, name) VALUES
+(100, 'プラハチャレンジ');
 -- ワークスペースの編集
 UPDATE workspaces
 SET name = 'プラハチャレンジ(7期)'
-WHERE id = :workspace_id;
+WHERE id = 100;
 -- ワークスペースの削除
 DELETE FROM workspaces
-WHERE id = :workspace_id;
+WHERE id = 100;
 
 -- チャンネルの作成
-INSERT INTO channels (workspace_id, name, description, is_private) VALUES
-(:workspace_id, '_review', '', false);
+INSERT INTO channels (id, workspace_id, name, description, is_private) VALUES
+(100, 1, '_review', '', false);
 -- チャンネルの編集
 UPDATE channels
 SET name = '# _review'
-WHERE id = :channel_id;
+WHERE id = 100;
 -- チャンネルの削除
 DELETE FROM channels
-WHERE id = :channel_id;
+WHERE id = 100;
 
 -- ワークスペースメンバーの追加
-INSERT INTO workspace_members (user_id, workspace_id, role) VALUES
-(:user_id, :workspace_id, 0);
+INSERT INTO workspace_members (id, user_id, workspace_id, role) VALUES
+(100, 2, 3, 0);
 -- ワークスペースメンバーの編集
 UPDATE workspace_members
 SET role = 1
-WHERE id = :workspace_member_id;
+WHERE id = 100;
 -- ワークスペースメンバーの削除
 DELETE FROM workspace_members
-WHERE id = :workspace_member_id;
+WHERE id = 100;
 
 -- チャンネルメンバーの追加
-INSERT INTO channel_members (user_id, channel_id) VALUES
-(:user_id, :channel_id);
+INSERT INTO channel_members (id, user_id, channel_id) VALUES
+(100, 4, 5);
 -- チャンネルメンバーの削除
 DELETE FROM channel_members
-WHERE id = :channel_member_id;
+WHERE id = 100;
 
 -- メッセージの作成
-INSERT INTO messages (user_id, channel_id, content) VALUES
-(:user_id, :channel_id, 'サンプルメッセージです！');
+INSERT INTO messages (id, user_id, channel_id, content) VALUES
+(100, 1, 1, 'サンプルメッセージです！');
 -- メッセージの編集
 UPDATE messages
 SET content = 'サンプルメッセージです！/n 編集しました！'
-WHERE id = :message_id;
+WHERE id = 100;
 -- メッセージの削除
 DELETE FROM messages
-WHERE id = :message_id;
+WHERE id = 100;
 
 -- スレットメッセージの作成
-INSERT INTO threads (parent_message_id, user_id, content) VALUES
-(:parent_message_id, :user_id, 'サンプルスレットメッセージです！');
+INSERT INTO threads (id, parent_message_id, user_id, content) VALUES
+(100, 1, 2, 'サンプルスレットメッセージです！');
 -- スレットメッセージの編集
 UPDATE threads
 SET content = 'サンプルスレットメッセージです！/n 編集しました！'
-WHERE id = :thread_id;
+WHERE id = 100;
 -- スレットメッセージの削除
 DELETE FROM threads
-WHERE id = :thread_id;
+WHERE id = 100;
 
 ---- SELECT
 -- messagesとthreadsの横断検索（文字）
@@ -88,25 +88,25 @@ ORDER BY created_at ASC;
 -- messagesとthreadsの横断検索（送信者）
 SELECT'message' as type, m.id, m.user_id, m.channel_id, m.content, m.created_at
 FROM messages m
-WHERE m.user_id = :user_id
+WHERE m.user_id = 1
 UNION ALL
 SELECT 'thread' as type, t.id, t.user_id, c.id, t.content, t.created_at
 FROM threads t
 JOIN messages parent_msg ON t.parent_message_id = parent_msg.id
 JOIN channels c ON parent_msg.channel_id = c.id
-WHERE t.user_id = :user_id
+WHERE t.user_id = 1
 ORDER BY created_at ASC;
 
 -- messagesとthreadsの横断検索（チャンネル）
 SELECT'message' as type, m.id, m.user_id, m.channel_id, m.content, m.created_at
 FROM messages m
-WHERE m.channel_id = :channel_id
+WHERE m.channel_id = 1
 UNION ALL
 SELECT 'thread' as type, t.id, t.user_id, c.id, t.content, t.created_at
 FROM threads t
 JOIN messages parent_msg ON t.parent_message_id = parent_msg.id
 JOIN channels c ON parent_msg.channel_id = c.id
-WHERE c.id = :channel_id
+WHERE c.id = 1
 ORDER BY created_at ASC;
 
 -- messagesとthreadsの横断検索（日時）
@@ -146,7 +146,7 @@ ORDER BY created_at ASC;
 -- 特定のmessageよりthreadのメッセージを取得
 SELECT * 
 FROM threads
-WHERE parent_message_id = :parent_message_id
+WHERE parent_message_id = 1
 ORDER BY created_at ASC;
 
 -- 特定のユーザーを想定して
@@ -154,18 +154,18 @@ ORDER BY created_at ASC;
     SELECT w.name
     FROM workspaces w 
     JOIN workspace_members members ON w.id = members.workspace_id
-    WHERE members.user_id = :user_id;
+    WHERE members.user_id = 1;
 
     -- 特定のワークスペースを選択した際に表示するchannelの取得
     SELECT c.name
     FROM channels c 
     JOIN channel_members members ON c.id = members.channel_id
-    WHERE members.user_id = :user_id
-    AND c.workspace_id = :workspace_id;
+    WHERE members.user_id = 1
+    AND c.workspace_id = 1;
 
 -- チャネルを開いた際を想定して
     -- 表示するメッセージの取得
     SELECT *
     FROM messages
-    WHERE channel_id = :channel_id
+    WHERE channel_id = 1
     ORDER BY created_at ASC;
